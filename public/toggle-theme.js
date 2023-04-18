3d7bf68150f7c8ebbcc1117d82dec18150b4c5b1
Samuel Gunter <samathingamajig@gmail.com>
2023-04-18 17:11:35 -0500

    Default to dark mode until explicit change

--- public/toggle-theme.js
+++ public/toggle-theme.js
@@ -1,4 +1,4 @@
-const primaryColorScheme = ""; // "light" | "dark"
+const primaryColorScheme = "dark"; // "light" | "dark"
 
 // Get theme data from local storage
 const currentTheme = localStorage.getItem("theme");
@@ -7,13 +7,7 @@ function getPreferTheme() {
   // return theme value in local storage if it is set
   if (currentTheme) return currentTheme;
 
-  // return primary color scheme if it is set
-  if (primaryColorScheme) return primaryColorScheme;
-
-  // return user device's prefer color scheme
-  return window.matchMedia("(prefers-color-scheme: dark)").matches
-    ? "dark"
-    : "light";
+  return primaryColorScheme;
 }
 
 let themeValue = getPreferTheme();
